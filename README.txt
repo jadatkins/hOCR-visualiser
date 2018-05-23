f0256bff8789048792d0c83c318f14599b1f6709
Alexander Atkins <jalexanderdatkins@gmail.com>
2018-05-23 12:46:49 +0100

    initial commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+1. Rename your .hocr file to .html.
+
+2. Add the following lines to the <head> tag:
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://raw.githubusercontent.com/jadatkins/hOCR-visualiser/master/visualise-hocr.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://raw.githubusercontent.com/jadatkins/hOCR-visualiser/master/visualise-hocr.css">
+
+3. View in a browser.
---
+++ visualise-hocr.js
@@ -0,0 +1,93 @@
+$(function() {
+    function parseTitle(text) {
+        var parts = text.split("; ");
+        for (var i = 0; i < parts.length; i++) {
+            var words = parts[i].split(" ");
+            if (words[0] == "bbox") {
+                var left   = parseInt(words[1]);
+                var top    = parseInt(words[2]);
+                var right  = parseInt(words[3]);
+                var bottom = parseInt(words[4]);
+                return {
+                    left:   Math.min(left, right),
+                    top:    Math.min(top, bottom),
+                    right:  Math.max(left, right),
+                    bottom: Math.max(top, bottom),
+                };
+            }
+        }
+    }
+
+    function isBlock(css_class) {
+        return [
+            "ocr_page",
+            "ocr_column",
+            "ocr_carea",
+            "ocr_float",
+            "ocr_textfloat",
+            "ocr_table",
+            "ocr_document",
+            "ocr_part",
+            "ocr_chapter",
+            "ocr_section",
+            "ocr_subsection",
+            "ocr_subsubsection",
+            "ocr_par",
+            "ocrx_block",
+        ].includes(css_class);
+    }
+
+    function visualise() {
+        var top_offset = 0;
+
+        $(".ocr_page").each(function(index, element) {
+            var ocr_page = $(this);
+            var page_bbox = parseTitle(ocr_page.attr("title"));
+            var scale = $(window).width() / page_bbox.right;
+
+            ocr_page.css("top", top_offset)
+                    .height(scale * (page_bbox.bottom - page_bbox.top));
+
+            ocr_page.find('[class*="ocr_"], [class*="ocrx_"]').each(function(index, element) {
+                var ocr_element = $(this);
+
+                element_bbox = parseTitle(ocr_element.attr("title"));
+                var elem_width = scale * (element_bbox.right - element_bbox.left);
+                var elem_height = scale * (element_bbox.bottom - element_bbox.top);
+
+                ocr_element.css("top", scale * element_bbox.top)
+                           .css("left", scale * element_bbox.left)
+                           .width(elem_width)
+                           .height(elem_height);
+
+                if (isBlock(ocr_element.attr("class"))) {
+                    return;
+                }
+
+                var font_size = Math.min(elem_width, 0.7 * elem_height);
+                var short = (ocr_element[0].innerText.length <= 2);
+                if (short) {
+                    font_size = 0.7 * elem_height;
+                }
+                if (font_size < 1) {
+                    ocr_element.css("font-size", "xx-small");
+                } else {
+                    ocr_element.css("font-size", font_size);
+                }
+
+                if (elem_height * 0.7 <= elem_width || short) {
+                    ocr_element.css("padding-top", 0.05 * elem_height)
+                               .css("word-break", "normal");
+                } else {
+                    ocr_element.css("text-align", "center")
+                               .css("word-break", "break-word");
+                }
+            });
+
+            top_offset += scale * page_bbox.bottom;
+        });
+    }
+
+    $(window).resize(visualise);
+    visualise();
+});
