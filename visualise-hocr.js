8e34bb1267626c19b596ac21e125ab55ff1ea165
Alexander Atkins <jalexanderdatkins@gmail.com>
2018-06-08 17:20:03 +0100

    somewhat handle sideways text

--- visualise-hocr.js
+++ visualise-hocr.js
@@ -1,9 +1,13 @@
 $(function() {
-    function parseTitle(text) {
+    function parseTitle(text, key) {
         var parts = text.split("; ");
         for (var i = 0; i < parts.length; i++) {
             var words = parts[i].split(" ");
-            if (words[0] == "bbox") {
+            if (words[0] == key) {
+                if (key != "bbox") {
+                    return words[1];
+                }
+
                 var left   = parseInt(words[1]);
                 var top    = parseInt(words[2]);
                 var right  = parseInt(words[3]);
@@ -16,6 +20,7 @@ $(function() {
                 };
             }
         }
+        return null;
     }
 
     function isBlock(css_class) {
@@ -42,7 +47,7 @@ $(function() {
 
         $(".ocr_page").each(function(index, element) {
             var ocr_page = $(this);
-            var page_bbox = parseTitle(ocr_page.attr("title"));
+            var page_bbox = parseTitle(ocr_page.attr("title"), "bbox");
             var scale = $(window).width() / page_bbox.right;
 
             ocr_page.css("top", top_offset)
@@ -51,7 +56,7 @@ $(function() {
             ocr_page.find('[class*="ocr_"], [class*="ocrx_"]').each(function(index, element) {
                 var ocr_element = $(this);
 
-                element_bbox = parseTitle(ocr_element.attr("title"));
+                element_bbox = parseTitle(ocr_element.attr("title"), "bbox");
                 var elem_width = scale * (element_bbox.right - element_bbox.left);
                 var elem_height = scale * (element_bbox.bottom - element_bbox.top);
 
@@ -59,6 +64,17 @@ $(function() {
                            .css("left", scale * element_bbox.left)
                            .width(elem_width)
                            .height(elem_height);
+
+                var angle = parseTitle(ocr_element.attr("title"), "textangle");
+                if (angle == "90" || angle == "270") {
+                    ocr_element.css("writing-mode", "vertical-rl")
+                               .css("text-orientation", "sideways");
+                }
+                if (angle == "90" || angle == "180") {
+                    // TODO: this requires position: absolute in order to work
+                    // i.e. every element positioned relative to its parent
+                    // ocr_element.css("transform", "rotate(0.5turn)");
+                }
 
                 if (isBlock(ocr_element.attr("class"))) {
                     return;
